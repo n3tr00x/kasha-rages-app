6b65d34fb4c0744af26ba8ba6a3cec80d807c622
Hubert Warchoł <n3tr00x99@gmail.com>
2022-09-14 00:50:29 +0200

    styled form and develop a app logic

--- js/app.js
+++ js/app.js
@@ -1,21 +1,66 @@
 import champions from './champions.js';
 
 const setPresentDay = () => {
-	document.querySelector('.date-input').valueAsDate = new Date();
+	document.querySelector('.input__field--date').valueAsDate = new Date();
 };
 
 const assignChampionsToDatalist = () => {
 	for (const iterator of champions) {
 		const option = document.createElement('option');
 		option.setAttribute('value', iterator);
-		document.querySelector('.champions').appendChild(option);
+		document.querySelector('#champions').appendChild(option);
 	}
-}
+};
+
+const dataHandler = () => {
+	const date = document.querySelector('.input__field--date');
+	const champion = document.querySelector('.input__field--champion');
+	const description = document.querySelector('.input__field--desc');
+
+	if (!checkChampionValidity(champion.value))
+		throw new Error('Wprowadzono błędną postać!');
+
+	return {
+		id: Date.now(),
+		date: date.valueAsDate.toISOString().slice(0, 10),
+		champion: champion.value,
+		description: description.value,
+	};
+};
+
+const submitData = async event => {
+	event.preventDefault();
+
+	try {
+		await fetch('https://sheetdb.io/api/v1/uprq1nzevlt46', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(dataHandler()),
+		});
+	} catch (error) {
+		console.error(error.message);
+	}
+
+	resetInputs();
+};
+
+const checkChampionValidity = value => {
+	if (champions.includes(value)) return true;
+
+	return false;
+};
+
+const resetInputs = () => {
+	document.querySelectorAll('.input__field').forEach(field => (field.value = ''));
+	setPresentDay();
+};
 
 const init = () => {
 	setPresentDay();
 	assignChampionsToDatalist();
-}
+	document.querySelector('.submit-btn').addEventListener('click', submitData);
+};
 
 init();
-
